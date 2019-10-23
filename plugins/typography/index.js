0e5a9ed8353495a737e1a7e90bdaa03e3d971997
RJ Spiker <spiker.rj@gmail.com>
2019-10-23 14:20:41 -0600

    another adjustment based on design feedback

--- plugins/typography/index.js
+++ plugins/typography/index.js
@@ -8,24 +8,24 @@ module.exports = function typographyPlugin() {
       const props = data.hProperties || (data.hProperties = {})
 
       if (node.depth === 1) {
-        data.id = 'g-type-display-1'
-        props.className = 'g-type-display-1'
-      }
-      if (node.depth === 2) {
         data.id = 'g-type-display-2'
         props.className = 'g-type-display-2'
       }
-      if (node.depth === 3) {
+      if (node.depth === 2) {
         data.id = 'g-type-display-3'
         props.className = 'g-type-display-3'
       }
-      if (node.depth === 4) {
+      if (node.depth === 3) {
         data.id = 'g-type-display-4'
         props.className = 'g-type-display-4'
       }
-      if (node.depth === 5) {
+      if (node.depth === 4) {
         data.id = 'g-type-display-5'
         props.className = 'g-type-display-5'
+      }
+      if (node.depth === 5) {
+        data.id = 'g-type-display-6'
+        props.className = 'g-type-display-6'
       }
       if (node.depth === 6) {
         data.id = 'g-type-label'
