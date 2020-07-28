595e2b6064c154c0fcaf8c35b2a405ce2de9ed1d
Jeff Escalante <jescalan@users.noreply.github.com>
2020-07-28 16:40:28 -0400

    add test

--- index.test.js
+++ index.test.js
@@ -6,6 +6,7 @@ it('api works as intended', () => {
   expect(remarkPlugins.typography).toBeTruthy()
   expect(remarkPlugins.includeMarkdown).toBeTruthy()
   expect(remarkPlugins.allPlugins().length).toBe(4)
-  // passes options to anchorLinks correctly
+  // passes options correctly
   expect(remarkPlugins.allPlugins({ anchorLinks: 'foo' })[1][1]).toBe('foo')
+  expect(remarkPlugins.allPlugins({ includeMarkdown: 'bar' })[0][1]).toBe('bar')
 })
