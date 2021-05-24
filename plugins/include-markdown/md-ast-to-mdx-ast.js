eb3a633bacf2c26d414bd5f82a58500c4cfdfd3a
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-05-24 13:13:37 -0400

    docs: add comment on md-ast-to-mdx-ast

--- plugins/include-markdown/md-ast-to-mdx-ast.js
+++ plugins/include-markdown/md-ast-to-mdx-ast.js
@@ -1,3 +1,16 @@
+/*
+
+NOTE:
+This file is swiped directly from @mdxjs/mdx's createMdxAstCompiler.
+ref: https://github.com/mdx-js/mdx/blob/510bae2580958598ae29047bf755b1a2ea26cf7e/packages/mdx/md-ast-to-mdx-ast.js
+
+I considered the possibility of using createMdxAstCompiler rather than remark-mdx on its own.
+however, crucially, we do NOT want to transform our AST into a MDXAST, we ONLY want to
+transform custom component nodes (ie HTML that is really JSX) into JSX nodes.
+So it felt duplicative, but necessary, to copypasta this utility in to meet our needs.
+
+*/
+
 const visit = require('unist-util-visit')
 const { isComment, getCommentContents } = require('@mdx-js/util')
 
